1ca2a5409c63d43437f4f18f2101199be82f76c3
ponc3138 <fponce@csumb.edu>
2024-11-18 17:52:21 -0800

    User Login

---
+++ BookHive/src/components/LoginForm/LoginFormStyles.js
@@ -0,0 +1,97 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '90%',
+    backgroundColor: 'transparent',
+    borderColor: 'rgba(255, 255, 255, 0.2)',
+    borderWidth: 2,
+    borderRadius: 12,
+    paddingVertical: 30,
+    paddingHorizontal: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 15,
+    margin: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '700',
+    marginBottom: 25,
+    textAlign: 'center',
+    color: '#2b86e2',
+    letterSpacing: 1,
+  },
+  inputBox: {
+    position: 'relative',
+    width: '100%',
+    marginVertical: 20,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    backgroundColor: 'transparent',
+    borderColor: 'rgba(255, 255, 255, 0.3)',
+    borderWidth: 2,
+    borderRadius: 40,
+    color: 'rgb(233, 29, 29)',
+    paddingHorizontal: 15,
+    fontSize: 16,
+  },
+  iconPlaceholder: { // Replaces the icon style when not using FontAwesome
+    position: 'absolute',
+    right: 20,
+    top: '50%',
+    transform: [{ translateY: -10 }],
+    fontSize: 18,
+    color: '#0b0b0b',
+  },
+  button: {
+    width: '100%',
+    height: 45,
+    backgroundColor: '#4f73f1',
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'rgb(18, 16, 16)',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  registerLink: {
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#070707',
+    marginTop: 20,
+  },
+  linkText: {
+    color: 'rgb(21, 20, 20)',
+    fontWeight: '600',
+  },
+  errorContainer: {
+    backgroundColor: '#f8d7da',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    width: '100%',
+  },
+  errorText: {
+    color: '#721c24',
+    marginBottom: 5,
+  },
+  testCredentials: {
+    color: '#721c24',
+    fontSize: 14,
+  },
+});
+
+export default styles;
